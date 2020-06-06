4fb298a05ea968e9d442fbfba08be15ca7e399ab
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-06-06 17:16:14 -0300

    add recipient foreign key

--- database/migrations/2020_06_06_201415_correspondences.php
+++ database/migrations/2020_06_06_201415_correspondences.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Correspondence extends Migration
+class Correspondences extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,14 @@ class Correspondence extends Migration
     public function up()
     {
         Schema::create('correspondences', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('recipient', 80);
             $table->string('street', 80);
             $table->string('cep', 8);
             $table->string('city', 60);
             $table->string('uf', 2);
             $table->string('status', 20);
+            $table->bigInteger('id_recipient')->unsigned();
             $table->foreign('id_recipient')->references('id')->on('recipients');
             $table->timestamp('created_at')->useCurrent();
         });
