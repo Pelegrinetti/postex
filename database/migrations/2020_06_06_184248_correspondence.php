630244fe170fa8f2e7a720ccc4e390bf8e1a2f88
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-06-06 15:58:54 -0300

    add correspondences migration

---
+++ database/migrations/2020_06_06_184248_correspondence.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Correspondence extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correspondences', function (Blueprint $table) {
+            $table->id();
+            $table->string('recipient', 80);
+            $table->string('street', 80);
+            $table->string('cep', 8);
+            $table->string('city', 60);
+            $table->string('uf', 2);
+            $table->string('status', 20);
+            $table->foreign('id_recipient')->references('id')->on('recipient');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('correspondences');
+    }
+}
