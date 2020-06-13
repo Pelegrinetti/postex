ec563fbff97c6ef5b69a9c8fbf024f904cb8251c
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-06-12 23:29:51 -0300

    add foreach in the select options

--- resources/views/correspondence/create.blade.php
+++ resources/views/correspondence/create.blade.php
@@ -54,35 +54,11 @@
                         </div>
                         <div class="col-1">
                             <label for="uf">* UF</label>
-                            <select id="if" name="uf" class="input-form" placeholder="teste" >
+                            <select id="uf" name="uf" class="input-form">
                                 <option value="">Selecione</option>
-                                <option value="AC">AC</option>
-                                <option value="AL">AL</option>
-                                <option value="AP">AP</option>
-                                <option value="AM">AM</option>
-                                <option value="BA">BA</option>
-                                <option value="CE">CE</option>
-                                <option value="DF">DF</option>
-                                <option value="ES">ES</option>
-                                <option value="GO">GO</option>
-                                <option value="MA">MA</option>
-                                <option value="MS">MS</option>
-                                <option value="MT">MT</option>
-                                <option value="MG">MG</option>
-                                <option value="PA">PA</option>
-                                <option value="PB">PB</option>
-                                <option value="PR">PR</option>
-                                <option value="PE">PE</option>
-                                <option value="PI">PI</option>
-                                <option value="RJ">RJ</option>
-                                <option value="RN">RN</option>
-                                <option value="RS">RS</option>
-                                <option value="RO">RO</option>
-                                <option value="RR">RR</option>
-                                <option value="SC">SC</option>
-                                <option value="SP">SP</option>
-                                <option value="SE">SE</option>
-                                <option value="TO">TO</option>
+                            @foreach ($uf_list as $uf)
+                                <option value="{{ $uf }}">{{ $uf }}</option>
+                            @endforeach
                              </select>
                             @error('uf')
                                 <div class="alert-error"><i class="fas fa-exclamation-triangle"></i> {{ $message }} </div>
@@ -105,7 +81,6 @@
                             @enderror
                         </div>
                     </div>
-                    <input type="hidden" name="status" value="pendente">
                 <input type="submit" value="Cadastrar" class="btn-new">
             </form>
         </section>
