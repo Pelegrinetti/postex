dac53a6ad69678410df411b76034894176eb64d8
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-06-13 18:29:39 -0300

    create external js file

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+/* Search correspondences*/
+
+$(document).ready(function() {
+    
+    var token = $('meta[name="csrf-token"]').attr('content');
+    
+    $('#search-recipient').keyup(function() {
+        $.ajax({
+            url: "correspondences/search",
+            type: "post",
+            data: {
+                "_token": token,
+                query: $(this).val()
+            },
+            dataType: 'json',
+            success: function(response) {
+                if (response.data.length !== 0) {
+                    $('tbody').html(response.data.map(function(recipient) {
+                        return `<tr>
+                                    <td>${recipient.id}</td>
+                                    <td>${recipient.recipient}</td>
+                                    <td>${recipient.street}</td>
+                                    <td>${recipient.number}</td>
+                                    <td>${recipient.neighborhood}</td>
+                                    <td>${recipient.city}</td>
+                                    <td>${recipient.uf}</td>
+                                    <td>${recipient.cep}</td>
+                                    <td><i class="fas fa-caret-down"></i></td>
+                                <tr>`;
+                    }));
+                } else {
+                    $('tbody').html(`<tr>
+                                        <td colspan="9">Nenhuma correspondência encontrada.</td>
+                                    </tr>`)
+                }
+            }
+        })
+    })
+})
